869bb8e0b7cbbf00bbcbdbbbfc61d73818ca9aa2
habibcodes <habib.codes@gmail.com>
2021-10-28 09:23:49 -0400

    fix issue with repeated hardcoded data by changing props in Appontment and Show component logic

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -17,9 +17,10 @@ export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      <header>{props.time}</header>
+      <header id={props.id}>{props.time} </header>
       {/* {textDisplay()} */}
-      {props.interview ? <Show student={props.interview} /> : <Empty />}
+      {/* {props.interview ? <Show student={props.interview} /> : <Empty />} */}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
     </article>
   );
 }
